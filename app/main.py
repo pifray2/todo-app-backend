62e486028990f79334fb29bf00631d8472e40f16
Arsen Miyzamov <arsen.miyzamov.01@mail.ru>
2026-09-07 19:45:56 +0600

    Integrated Alembic migrations

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routers.category import router as category_router
 from app.api.routers.task import router as task_router
-from app.db.session import engine
-from app.models.base import Base
 
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-app = FastAPI(lifespan = lifespan)
+app = FastAPI()
 app.include_router(router=task_router)
 app.include_router(router=category_router)
 
